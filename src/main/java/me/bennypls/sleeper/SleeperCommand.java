8eccc1a210de5e1b9d3928e1d38fb8fb51e46e3a
BennyPLS <peptoni0345@gmail.com>
2023-04-16 01:43:41 +0200

    Version 0.9.2 HELP SUB-COMMAND
    This is an addition to sleeper command now you can get help in-game with the '/sleeper help' command.

--- src/main/java/me/bennypls/sleeper/SleeperCommand.java
+++ src/main/java/me/bennypls/sleeper/SleeperCommand.java
@@ -12,6 +12,38 @@ public class SleeperCommand implements CommandExecutor {
     private final Resting resting;
     private final Configuration configuration;
 
+    private static final String GENERAL_HELP = """
+        This is a command for managing the sleeper plugin.
+                            
+        The command has several sub-commands, each of which performs a different action.
+        The sub-commands are :
+        · skip      · allows administrators to skip the night and advance to sunrise.
+        · reload    · allows administrators to reload the configuration of the sleeping plugin.
+        · ignored   · allows administrators to add or remove players from a list of ignored players.
+        · help      · get help for the general command or especific sub-command.
+            
+        For specific help type (/sleeper help [sub-command] )
+        """;
+
+    private static final String SKIP_HELP = """
+        This is a sub-command of sleeper that allows administrators skip a night and advance to sunrise.
+        There needs to be at least 1000 ticks of time difference between the sunrise time (23850) to execute.
+        
+        WARNING : DO NOT EXECUTE MULTIPLE TIMES RAPIDLY WITH ANIMATION ENABLED CAN CAUSE PROBLEMS.
+        """;
+
+    private static final String RELOAD_HELP = """
+        This is a sub-command of sleeper that allows administrators to hot reload the sleeper configuration.
+        """;
+
+    private static final String IGNORED_HELP = """
+        This is a sub-command of sleeper that allows administrators to add or remove players from a list of ignored players.
+        
+        Usage : /sleeper ignored [add | remove] (PLAYER USERNAME)
+        
+        These players will not be counted towards the total number of players needed to sleep through the night.
+        """;
+
     public SleeperCommand(JavaPlugin plugin, Resting resting, Configuration configuration) {
         this.plugin = plugin;
         this.resting = resting;
@@ -19,24 +51,28 @@ public class SleeperCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String commandName, String[] commandArguments) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (commandArguments.length == 0) {
-            commandSender.sendMessage("This command need at least 1 argument.");
+        if (args.length == 0) {
+            sender.sendMessage("This command need at least 1 argument.");
             return false;
         }
 
-        switch (commandArguments[0].toLowerCase()) {
+        switch (args[0].toLowerCase()) {
             case "skip" -> {
-                return skip(commandSender, commandArguments);
+                return skip(sender, args);
             }
 
             case "reload" -> {
-                return reload(commandSender, commandArguments);
+                return reload(sender, args);
             }
 
             case "ignored" -> {
-                return ignoredPlayers(commandSender, commandArguments);
+                return ignoredPlayers(sender, args);
+            }
+
+            case "help" -> {
+                return help(sender, args);
             }
 
             default -> {
@@ -45,12 +81,12 @@ public class SleeperCommand implements CommandExecutor {
         }
     }
 
-    private boolean skip(CommandSender commandSender, String[] commandArguments) {
-        if (!(commandSender instanceof Player player)) {
+    private boolean skip(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player player)) {
             return false;
         }
 
-        if (commandArguments.length != 1) {
+        if (args.length != 1) {
             player.sendMessage("Unnecessary arguments");
             return false;
         }
@@ -62,70 +98,101 @@ public class SleeperCommand implements CommandExecutor {
         return true;
     }
 
-    private boolean reload(CommandSender commandSender, String[] commandArguments) {
-        if (commandArguments.length != 1) {
-            commandSender.sendMessage("Unnecessary arguments");
+    private boolean reload(CommandSender sender, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("Unnecessary arguments");
             return false;
         }
 
-        commandSender.sendMessage("Sleeper : Starting configuration reload...");
+        sender.sendMessage("Sleeper : Starting configuration reload...");
         configuration.reload();
-        commandSender.sendMessage("Sleeper : Reload completed!");
+        sender.sendMessage("Sleeper : Reload completed!");
 
         return true;
     }
 
-    public boolean ignoredPlayers(CommandSender commandSender, String[] commandArguments) {
-        if (commandArguments.length != 3) {
-            commandSender.sendMessage("This command needs 3 arguments");
+    public boolean ignoredPlayers(CommandSender sender, String[] args) {
+        if (args.length != 3) {
+            sender.sendMessage("This command needs 3 arguments");
             return false;
         }
 
-        Player target = commandSender.getServer().getPlayer(commandArguments[2]);
+        Player target = sender.getServer().getPlayer(args[2]);
 
         if (target == null) {
-            commandSender.sendMessage(commandSender.getServer().getOnlinePlayers().toString());
-            commandSender.sendMessage("The player does not exist");
+            sender.sendMessage(sender.getServer().getOnlinePlayers().toString());
+            sender.sendMessage("The player does not exist");
             return false;
         }
 
-        switch (commandArguments[1].toLowerCase()) {
+        switch (args[1].toLowerCase()) {
             case "add" -> {
-                return add(commandSender, target.getName());
+                return add(sender, target.getName());
             }
 
             case "remove" -> {
-                return remove(commandSender, target.getName());
+                return remove(sender, target.getName());
             }
 
             default -> {
-                commandSender.sendMessage(commandArguments[1] + " is not a valid action.");
+                sender.sendMessage(args[1] + " is not a valid action.");
                 return false;
             }
         }
     }
 
-    private boolean add(CommandSender commandSender, String playerName) {
+    private boolean add(CommandSender sender, String playerName) {
         boolean success = configuration.addIgnoredPlayer(playerName);
 
         if (success) {
-            commandSender.sendMessage("The player - " + playerName + " - has been added to the ignored list.");
+            sender.sendMessage("The player - " + playerName + " - has been added to the ignored list.");
         } else {
-            commandSender.sendMessage("The player with name - " + playerName + " - is already in the ignored list.");
+            sender.sendMessage("The player with name - " + playerName + " - is already in the ignored list.");
         }
 
         return success;
     }
 
-    private boolean remove(CommandSender commandSender, String playerName) {
+    private boolean remove(CommandSender sender, String playerName) {
         boolean success = configuration.removeIgnoredPlayer(playerName);
 
         if (success) {
-            commandSender.sendMessage("The player - " + playerName + " - has been removed from the ignored list.");
+            sender.sendMessage("The player - " + playerName + " - has been removed from the ignored list.");
         } else {
-            commandSender.sendMessage("The player with name - " + playerName + " - not found in ignored list.");
+            sender.sendMessage("The player with name - " + playerName + " - not found in ignored list.");
         }
 
         return success;
+    }
+
+    private boolean help(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            sender.sendMessage(GENERAL_HELP);
+            return true;
+        }
+
+        switch (args[1]) {
+
+            case "skip" -> {
+                sender.sendMessage(SKIP_HELP);
+                return true;
+            }
+
+            case "reload" -> {
+                sender.sendMessage(RELOAD_HELP);
+                return true;
+            }
+
+            case "ignored" -> {
+                sender.sendMessage(IGNORED_HELP);
+                return true;
+            }
+
+            default -> {
+                sender.sendMessage("This is not a valid sub-command to get help of.");
+                return false;
+            }
+        }
+
     }
 }
